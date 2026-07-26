a435b322ef4a9d742d302d230555f534fc877c5b
MD SHAIFULLAH KHAN <khanshaif9102@gmail.com>
2026-07-26 19:49:18 +0530

    added the functionality of category create

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,36 @@ namespace BulkyBookWeb.Controllers
         {
             var categories = await _context.Categories.ToListAsync();
             return View("Index", categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public async Task<IActionResult> CreatePost(Category category)
+        {
+            if (!string.IsNullOrEmpty(category.Name) && _context.Categories.Any(c => c.Name.Trim().ToLower() == category.Name.Trim().ToLower()))
+            {
+                ModelState.AddModelError("", "Category name is allready exists! ");
+            }
+            if (ModelState.IsValid)
+            {
+                await _context.Categories.AddAsync(category);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
         }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
-        public int DisplayOrder { get; set; }
+        [Range(0,100,ErrorMessage ="Range must be between 0 and 100")]
+        public int? DisplayOrder { get; set; }
     }
 }
