020b6570ac8422997c5b1df08bb235e4e8567d5f
MD SHAIFULLAH KHAN <khanshaif9102@gmail.com>
2026-07-26 13:29:39 +0530

    added view and laout and styling

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View("Index", categories);
+        }
+    }
+}
--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -9,5 +9,15 @@ namespace BulkyBookWeb.Data
 
         public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name ="Action",DisplayOrder = 1},
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
