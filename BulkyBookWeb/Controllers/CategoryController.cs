89cb287b1da00731c5a0f7d97561a0b9b004e6ab
MD SHAIFULLAH KHAN <khanshaif9102@gmail.com>
2026-07-27 07:07:14 +0530

    added all functionality for category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -37,16 +37,76 @@ namespace BulkyBookWeb.Controllers
             {
                 await _context.Categories.AddAsync(category);
                 await _context.SaveChangesAsync();
-
+                TempData["success"] = "Category has been created successfully";
                 return RedirectToAction("Index");
             }
             else
                 return View();
         }
 
-        public IActionResult Update()
+        public async Task<IActionResult> Update(int? id)
         {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Update")]
+        public async Task<IActionResult> UpdatePOST(Category category)
+        {
+            if (!string.IsNullOrEmpty(category.Name) && await _context.Categories.AnyAsync(c => c.Name.Trim().ToLower() == category.Name.Trim().ToLower() && c.Id != category.Id))
+            {
+                ModelState.AddModelError("", "Category name is allready exists! ");
+            }
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Category has been updated successfully";
+                return RedirectToAction("Index");
+            }
             return View();
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePOST(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "Category has been deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
