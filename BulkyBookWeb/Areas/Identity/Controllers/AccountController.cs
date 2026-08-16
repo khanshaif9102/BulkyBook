0aca0685fc2c31776574baf6ec11e0d2f9cd04b6
MD SHAIFULLAH KHAN <khanshaif9102@gmail.com>
2026-08-16 15:24:43 +0530

    added login functionality

--- BulkyBookWeb/Areas/Identity/Controllers/AccountController.cs
+++ BulkyBookWeb/Areas/Identity/Controllers/AccountController.cs
@@ -24,7 +24,17 @@ namespace BulkyBookWeb.Areas.Identity.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home", new { area = "Customer" });
+                }
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(loginVM);
         }
 
 
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -16,7 +16,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 });
 
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
